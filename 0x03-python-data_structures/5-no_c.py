78f0b9524522774d2e6e71aee1f59adfb46d715d
rumonah <rummaloba@gmail.com>
2024-01-06 00:15:59 -0800

    ommit c

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    new_string = " "
+    new_string = ""
     for elements in my_string:
         if elements != "c" and elements != "C":
             new_string += elements
