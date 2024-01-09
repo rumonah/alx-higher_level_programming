33db71a85e3ad49f1719c831533eb90cab453012
rumonah <rummaloba@gmail.com>
2024-01-09 15:26:39 -0800

    calculate

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -4,7 +4,8 @@ if __name__ == "__main__":
     from calculator_1 import add, sub, mul, div
     argc = len(argv)
     if argc != 4:
-        print('Usage: ./100-my_calculator.py <a> <operator> <b>'.format(argv[0]))
+        print('Usage: ./100-my_calculator.py <a> 
+                <operator> <b>'.format(argv[0]))
         exit(1)
     oper = {
             '+': add,
@@ -16,7 +17,7 @@ if __name__ == "__main__":
         calc1 = int(argv[1])
         calc2 = int(argv[3])
         ops = oper[argv[2]]
-        res= ops(calc1, calc2)
+        res = ops(calc1, calc2)
         print('{:d} {:s} {:d} = {:d}'.format(calc1, argv[2], calc2, res))
     else:
         print('Unknown operator. Available operators: +, -, * and /')
