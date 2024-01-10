27e484dbab912fa418459f27afc45f9efd019e33
rumonah <rummaloba@gmail.com>
2024-01-10 15:34:57 -0800

    fixed sorting dictonary

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -2,4 +2,4 @@
 def print_sorted_dictionary(a_dictionary):
     x = sorted(a_dictionary)
     for r in x:
-        print("{}:{}".format(r, a_dictionary[r]))
+        print("{}: {}".format(r, a_dictionary[r]))
