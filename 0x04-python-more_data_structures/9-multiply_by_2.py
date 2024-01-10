f39545c8f52eb85d6d5d022e7927147a51388a2d
rumonah <rummaloba@gmail.com>
2024-01-10 14:00:48 -0800

    fixed

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,6 +1,6 @@
-#!/bin/bash/python3
+#!/usr/bin/python3
 def multiply_by_2(a_dictionary):
     rumzy_dict = dict(a_dictionary)
     for r, x in rumzy_dict.items():
         rumzy_dict[r] = x * 2
-        return  rumzy_dict
+        return rumzy_dict
