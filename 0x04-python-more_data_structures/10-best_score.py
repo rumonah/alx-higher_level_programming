887410a9e54b8cb7d27c9d0946011b693c367d2c
rumonah <rummaloba@gmail.com>
2024-01-10 15:56:00 -0800

    best score

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,11 +1,3 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if not a_dictionary:
-        return None
-    Max = 0
-    for key, value in a_dictionary.items():
-        if value > Max:
-            Max = value
-            for key, value in a_dictionary.item():
-                if value == Max:
-                    return key
+    return max(a_dictionary, key=a_dictionary.get)if a_dictionary else None
