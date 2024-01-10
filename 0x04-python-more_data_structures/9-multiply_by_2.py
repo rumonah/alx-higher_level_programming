983a72060077797c7a5051622812cc13183eaf45
rumonah <rummaloba@gmail.com>
2024-01-10 13:39:36 -0800

    multiply

---
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -0,0 +1,6 @@
+#!/bin/bash/python3
+def multiply_by_2(a_dictionary):
+    rumzy_dict = dict(a_dictionary)
+    for r, x in rumzy_dict.items():
+        rumzy_dict[r] = x * 2
+        return  rumzy_dict
