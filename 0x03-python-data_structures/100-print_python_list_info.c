d918facf385e6fe4fdb2ed1fdd09cc5a6551337a
rumonah <rummaloba@gmail.com>
2024-01-13 07:03:44 -0800

    fixes

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -2,8 +2,8 @@
 #include <stdlib.h>
 #include <stddef.h>
 #include "python.h"
-#include <object.h>
-#include <listobject.h>
+#include "object.h"
+#include "listobject.h"
 
 /**
  * print_python_list_info - function that prints some
@@ -15,9 +15,9 @@ void print_python_list_info(PyObject *p)
 {
 	int r;
 
-	printf("[*] Size of the Python List = %lu\n", Py_Size);
-	printf("[*] Allocate = %lu\n", ((PyListObject *)p)->allocated);
+	printf("[*] Size of the Python List = %li\n", Py_Size);
+	printf("[*] Allocated = %li\n", ((PyListObject *)p)->allocated);
 
 	for (r = 0 ; r < Py_size(p) ; r++)
-		printf("Element %d: %s\n", r, Py_Type(Pylist_GetItem(p, r))->tp_name);
+		printf("Element %i: %s\n", r, Py_Type(Pylist_GetItem(p, r))->tp_name);
 }
--- 0x03-python-data_structures/python.h
+++ 0x03-python-data_structures/python.h
@@ -4,6 +4,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stddef.h>
+#include "object.h"
+#include "listobject.h"
 
 void print_python_list_info(PyObject *p);
 
