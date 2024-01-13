3a6501d3ad5437133f56710341c7b30ad8e92710
rumonah <rummaloba@gmail.com>
2024-01-13 09:46:07 -0800

    its ok i guess

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -1,25 +1,16 @@
-#!/usr/include/python3.8/pymem.h:8
-#!/usr/include/python3.8/object.h:4,
-#include <stdio.h>
-#include <stdlib.h>
-#include <stddef.h>
-#include "python.h"
-#include "object.h"
-#include "listobject.h"
+#include <Python.h>
+#include <object.h>
+#include <listobject.h>
 
-/**
- * print_python_list_info - function that prints some
- * basic info about Python lists
- * @p: A PyObject list
- *
- */
 void print_python_list_info(PyObject *p)
 {
+	long int size = PyList_Size(p);
 	int r;
+	PyListObject *obj = (PyListObject *)p;
 
-	printf("[*] Size of the PyList_Size = %lu\n", Py_Size);
-	printf("[*] Allocated = %lu\n", ((PyListObject *)p)->allocated);
-
-	for (r = 0 ; r < Py_size(p) ; r++)
-		printf("Element %i: %s\n", r, Py_Type(Pylist_GetItem(p, r))->tp_name);
+	printf("[*] Size of the Python List = %li\n", size);
+	printf("[*] Allocated = %li\n", obj->allocated);
+	for (r = 0; r < size;ri++)
+		printf("Element %i: %s\n", r, Py_TYPE(obj->ob_item[r])->tp_name);
 }
+
