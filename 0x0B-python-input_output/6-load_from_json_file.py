2d8451d58259a593a2797918af1ca13953361d88
rumonah <rummaloba@gmail.com>
2024-02-06 09:52:24 -0800

    creates an Object from a “JSON file

---
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Defines a JSON file-reading function."""
+import json
+
+def load_from_json_file(filename):
+    """Create a Python object from a JSON file."""
+    with open(filename) as r:
+        return json.load(r)
